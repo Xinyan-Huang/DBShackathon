af9f865865c930e25d263a85e10f213947d84177
Low Kang Ngee <kangngee62@gmail.com>
2024-01-20 13:32:04 +0800

    Reuse same instance of db

--- backend/src/routes/Dashboard.js
+++ backend/src/routes/Dashboard.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const db = require('../../db');
 
 const router = express.Router();
 
@@ -8,29 +8,13 @@ router.get("/", (req, res) => {
   const user = req.body;
   console.log(user);
 
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "techtrek24",
-  });
-
-  con.connect(function (err) {
-    if (err) throw err;
-
-    console.log("Connected to DB");
-
-    con.query(
-      `SELECT i.id, i.title, c.name, i.budget, d.country_id, d.cost, d.name, d.notes FROM itinerary i LEFT JOIN country c ON i.country_id = c.id LEFT JOIN itinerary_destination id ON i.id = id.itinerary_id LEFT JOIN destination d ON id.destination_id = d.id WHERE i.user_id = ${req.body["user_id"]}`,
-      function (err, db_itineraries) {
-        if (err) console.log(err);
-        console.log(db_itineraries);
-        res.send(db_itineraries);
-
-        con.end();
-      }
-    );
-  });
+  db.query(
+    `SELECT i.id, i.title, c.name, i.budget, d.country_id, d.cost, d.name, d.notes FROM itinerary i LEFT JOIN country c ON i.country_id = c.id LEFT JOIN itinerary_destination id ON i.id = id.itinerary_id LEFT JOIN destination d ON id.destination_id = d.id WHERE i.user_id = ${req.body["user_id"]}`,
+    function (err, db_itineraries) {
+      if (err) console.log(err);
+      console.log(db_itineraries);
+      res.send(db_itineraries);
+    });
 });
 
 module.exports = router;
