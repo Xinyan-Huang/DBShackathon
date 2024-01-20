b413a898fe84d9591235dd14c282a85c4296d998
Tan Wen Xuan <wenxuantan97@gmail.com>
2024-01-20 13:52:27 +0800

    logic change

--- backend/src/routes/Dashboard.js
+++ backend/src/routes/Dashboard.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require('../../db');
+const db = require("../../db");
 
 const router = express.Router();
 
@@ -9,12 +9,13 @@ router.get("/", (req, res) => {
   console.log(user);
 
   db.query(
-    `SELECT i.id, i.title, c.name, i.budget, d.country_id, d.cost, d.name, d.notes FROM itinerary i LEFT JOIN country c ON i.country_id = c.id LEFT JOIN itinerary_destination id ON i.id = id.itinerary_id LEFT JOIN destination d ON id.destination_id = d.id WHERE i.user_id = ${req.body["user_id"]}`,
+    `SELECT i.id, i.title, c.name, i.budget, d.country_id, d.cost, d.name as destination, d.notes FROM itinerary i LEFT JOIN country c ON i.country_id = c.id LEFT JOIN itinerary_destination id ON i.id = id.itinerary_id LEFT JOIN destination d ON id.destination_id = d.id WHERE i.user_id = ${req.body["user_id"]}`,
     function (err, db_itineraries) {
       if (err) console.log(err);
       console.log(db_itineraries);
       res.send(db_itineraries);
-    });
+    }
+  );
 });
 
 module.exports = router;
