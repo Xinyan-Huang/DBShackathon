72f756bdf21d31343d8c1595bf0d69583a4407f4
xxianhuii <keexianhui@gmail.com>
2024-01-20 13:07:42 +0800

    wip

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
-var cookieParser = require("cookie-parser");
+const cors = require('cors');
+var cookieParser = require('cookie-parser');
 app.use(express.json());
 // const db = require('./db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const mysql = require("mysql");
 const mysql = require('mysql');
 const destinationRouter = require('./src/routes/DestinationRouter');
 
 //authentication
 function authenticate(req, res, next) {
-    const auth = req.headers.authorization;
-    const token = auth && auth.split(" ")[1];
-    console.log(token, "HI");
-    console.log(auth)
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        console.log(user, "user");
-        next();
-    });
+  const auth = req.headers.authorization;
+  const token = auth && auth.split(' ')[1];
+  console.log(token, 'HI');
+  console.log(auth);
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    console.log(user, 'user');
+    next();
+  });
 }
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use('/router/destination', destinationRouter);
 app.use(cookieParser());
 
 //Backend Listens to port 5001, your axios calls should be localhost:5001
-app.listen(5001, () => console.log("Server up and running... on port 5001"));
+app.listen(5001, () => console.log('Server up and running... on port 5001'));
