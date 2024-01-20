81f35378d9910acabc6769236d1bac95803f17b0
syongkheng <33014950+syongkheng@users.noreply.github.com>
2024-01-20 13:41:51 +0800

    chore: refactor login endpoint for frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ app.use(express.json());
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const mysql = require("mysql");
-const dbroutes = require("./src/routes/Dashboard.js");
+const dbRoutes = require("./src/routes/Dashboard.js");
 const itineraryRoutes = require('./src/routes/ItineraryRouter')
-const loginroute = require("./src/routes/LoginRouter")
+const loginRoute = require("./src/routes/LoginRouter")
 
 //authentication
 function authenticate(req, res, next) {
@@ -29,9 +29,9 @@ function authenticate(req, res, next) {
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 
-app.use("/db", dbroutes);
+app.use("/db", dbRoutes);
 app.use('/itinerary', itineraryRoutes)
-app.use("/login", loginroute);
+app.use("/login", loginRoute);
 
 //Backend Listens to port 5001, your axios calls should be localhost:5001
 app.listen(5001, () => console.log("Server up and running... on port 5001"));
