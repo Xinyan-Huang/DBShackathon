60c1bf49de44835b925c41b0befce0122c1cc476
Tan Wen Xuan <wenxuantan97@gmail.com>
2024-01-20 11:52:15 +0800

    logic update

--- backend/src/routes/Dashboard.js
+++ backend/src/routes/Dashboard.js
@@ -1,4 +1,9 @@
-router.post("/", (req, res) => {
+const express = require("express");
+const mysql = require("mysql");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
   console.log("POST Route");
   const user = req.body;
   console.log(user);
@@ -9,4 +14,23 @@ router.post("/", (req, res) => {
     password: "password",
     database: "techtrek24",
   });
+
+  con.connect(function (err) {
+    if (err) throw err;
+
+    console.log("Connected to DB");
+
+    con.query(
+      "SELECT i.id, i.title, c.name, i.budget FROM techtrek24.itinerary i LEFT JOIN country c ON i.country_id = c.id",
+      function (err, db_itineraries) {
+        if (err) console.log(err);
+        console.log(db_itineraries);
+        res.send(db_itineraries);
+
+        con.end();
+      }
+    );
+  });
 });
+
+module.exports = router;
