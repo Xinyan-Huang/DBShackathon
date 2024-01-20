de7ca15a3467222fad166b4a9be7a64122e75ca1
josiahfoo99 <90398774+josiahfoo99@users.noreply.github.com>
2024-01-20 15:14:47 +0800

    Removed duplicated line

--- backend/index.js
+++ backend/index.js
@@ -37,4 +37,3 @@ app.use('/router/destination', destinationRouter);
 
 //Backend Listens to port 5001, your axios calls should be localhost:5001
 app.listen(5001, () => console.log("Server up and running... on port 5001"));
-app.listen(5001, () => console.log('Server up and running... on port 5001'));
